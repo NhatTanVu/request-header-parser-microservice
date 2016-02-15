81d7d30bf99ec49decc0fc0ca9aeff5b0beca94f
tan_vu <onchua2006@gmail.com>
2016-02-15 03:46:39 +0000

    Add service code

---
+++ README.md
@@ -0,0 +1,21 @@
+# API Basejump: Request Header Parser microservice
+
+## User stories
+
+1. I can get the IP address, language and operating system for my browser.
+
+## Example usage
+
+```
+https://request-header-parser-microservice-tan-vu-1.c9users.io/api/whoami
+```
+
+## Example output
+
+```
+{ "ipaddress":"14.161.67.118", "language":"en-US", "software":"Windows 10.0" }
+```
+
+## License
+
+N/A
---
+++ server.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var http = require('http')
+  , useragent = require('express-useragent')
+  , accepts = require('accepts')
+  , url = require("url");
+
+var srv = http.createServer(function (req, res) {
+    var pathname = url.parse(req.url).pathname;
+    if (pathname == "/api/whoami") {
+      	var source = req.headers['user-agent'],
+      	ua = useragent.parse(source);
+      	var ip = req.headers['x-forwarded-for'].split(",")[0] || 
+    		req.connection.remoteAddress || 
+         	req.socket.remoteAddress ||
+         	req.connection.socket.remoteAddress;
+        var language = accepts(req).languages()[0]; 
+      
+      	var result = {};
+      	result.ipaddress = ip;
+      	result.language = language;
+      	result.software = ua.os;
+      
+      	res.writeHead(200, {'Content-Type': 'text/plain'});
+      	res.end(JSON.stringify(result));
+    }
+    else if (pathname == "/") {
+        res.writeHead(302, {
+            'Location': '/api/whoami'
+        });
+        res.end();
+    }
+    else {
+        res.writeHead(404);
+        res.end();
+    }
+});
+
+srv.listen(8080);
